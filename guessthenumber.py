87b8afcc8a498281e4fc947220368968ed06ccad
MichelleArumemi <arumemi.michelle@binghamuni.edu.ng>
2024-01-19 16:02:00 +0100

    feat: Update guessing game parameters
    - Changed number range to 10-50
    - Added limited user attempts
    - Print out the generated number
    - Included comments for clarity

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,24 +1,42 @@
 import random
 
-# Generate a random number between 1 and 10
-generatedNumber = random.randrange(1, 10)
+userAttempts = 3
+# Generate a random number between 1 and 50
+generatedNumber = random.randrange(1, 51)
 
 # Welcome message
-print("Welcome to guess the number! The computer generates a random number, and YOU, the user, have to guess it.")
-
+print("Welcome to Guess The Number! The computer generates a random number, and YOU, the user, has to guess it.")
 try:
-    # Get user input as an integer
-    userInput = int(input("Guess a number between 1-10: "))
+    while userAttempts > 0:
+        # Get user input as an integer
+        userInput = int(input("Guess a number between 1 and 50: "))
 
-    # Check if the user input is greater than 10
-    if userInput > 10:
-        print("This number is greater than 10!")
-    else:
         # Check if the user's guess is correct
         if userInput == generatedNumber:
             print("You guessed correctly! Here's a cookie 🍪")
-        else:
-            print("Incorrect guess. Try again ☹️")
+            break
+
+        # Check if the user input is greater than the generated number
+        elif userInput > generatedNumber:
+            userAttempts -= 1
+            if userAttempts > 0:
+                print("This number is too high. Try again.")
+                print(f"You have {userAttempts} attempts left")
+            else:
+                print("You lose.")
+                print(f"The number was {generatedNumber}")
+                break
+
+        # Check if the user input is less than the generated number
+        elif userInput < generatedNumber:
+            userAttempts -= 1
+            if userAttempts > 0:
+                print("This number is too low. Try again.")
+                print(f"You have {userAttempts} attempts left")
+            else:
+                print("You lose.")
+                print(f"The number was {generatedNumber}")
+                break
 
 except ValueError:
     # Handle the case where the user enters a non-integer value
